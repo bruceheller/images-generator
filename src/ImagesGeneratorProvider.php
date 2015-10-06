60914b8701d01aa21421416390935ba154e2b436
Bruce Heller <bheller@bheller.com>
2015-10-06 22:11:12 +0200

    Documentation and username changes

--- src/ImagesGeneratorProvider.php
+++ src/ImagesGeneratorProvider.php
@@ -1,12 +1,12 @@
 <?php
-
-namespace DJLemmings\ImagesGenerator;
+namespace bheller\ImagesGenerator;
 
 /**
  * Provider for the Faker generator
  */
 class ImagesGeneratorProvider extends \Faker\Provider\Base
 {
+
     /**
      * Generate a new image to disk and return its location
      *
@@ -14,19 +14,27 @@ class ImagesGeneratorProvider extends \Faker\Provider\Base
      *
      * @example '/path/to/dir/13b73edae8443990be1aa8f1a483bc27.jpg'
      *
-     * @param string $dir Path of the generated file
-     * @param integer $width Width of the picture in pixels
-     * @param integer $height Height of the picture in pixels
-     * @param string $format Image format, jpg or png. Default as jpg
-     * @param bool $fullPath Return full pathfile if true
-     * @param string $word Text to generate on the picture, default to width and height.
-     * @param string $backgroundColor Background color in hexadecimal format (eg. #7f7f7f), default to black
-     * @param string $textColor Text color in hexadecimal format, default to white
+     * @param string $dir
+     *            Path of the generated file, if null will use the system temp dir
+     * @param integer $width
+     *            Width of the picture in pixels
+     * @param integer $height
+     *            Height of the picture in pixels
+     * @param string $format
+     *            Image format, jpg or png. Default as jpg
+     * @param bool $fullPath
+     *            Return full pathfile if true
+     * @param string $text
+     *            Text to generate on the picture, default no text, if true given will output width and height.
+     * @param string $backgroundColor
+     *            Background color in hexadecimal format (eg. #7f7f7f), default to black
+     * @param string $textColor
+     *            Text color in hexadecimal format, default to white
      */
-    public static function imageGenerator($dir = null, $width = 640, $height = 480, $format = 'jpg', $fullPath = true, $word = null, $backgroundColor = null, $textColor = null)
+    public static function imageGenerator($dir = null, $width = 640, $height = 480, $format = 'jpg', $fullPath = true, $text = null, $backgroundColor = null, $textColor = null)
     {
         $dir = is_null($dir) ? sys_get_temp_dir() : $dir; // GNU/Linux / OS X / Windows compatible
-        // Validate directory path
+                                                          // Validate directory path
         if (! is_dir($dir) || ! is_writable($dir)) {
             throw new \InvalidArgumentException(sprintf('Cannot write to directory "%s"', $dir));
         }
@@ -46,25 +54,27 @@ class ImagesGeneratorProvider extends \Faker\Provider\Base
                 imagecolorallocate($image, 0, 0, 0);
             }
 
-            if (is_null($word)) {
-                $word = $width . 'x' . $height;
+            if ($text === true) {
+                $text = $width . 'x' . $height;
             }
 
-            if ($textColor) {
-                $rgb = str_split(substr($textColor, 1), 2);
-                $text_color = imagecolorallocate($image, hexdec($rgb[0]), hexdec($rgb[1]), hexdec($rgb[2]));
-            } else {
-                $text_color = imagecolorallocate($image, 255, 255, 255);
-            }
+            if (! is_null($text)) {
+                if ($textColor) {
+                    $rgb = str_split(substr($textColor, 1), 2);
+                    $text_color = imagecolorallocate($image, hexdec($rgb[0]), hexdec($rgb[1]), hexdec($rgb[2]));
+                } else {
+                    $text_color = imagecolorallocate($image, 255, 255, 255);
+                }
 
-            $fontSize = 200;
-            $textBoundingBox = imagettfbbox($fontSize, 0, __DIR__ . '/font/Roboto-Regular.ttf', $word);
-            // decrease the default font size until it fits nicely within the image - Code adapted from https://github.com/img-src/placeholder
-            while (((($width - ($textBoundingBox[2] - $textBoundingBox[0])) < 10) || (($height - ($textBoundingBox[1] - $textBoundingBox[7])) < 10)) && ($fontSize > 1)) {
-                $fontSize --;
-                $textBoundingBox = imagettfbbox($fontSize, 0, __DIR__ . '/font/Roboto-Regular.ttf', $word);
+                $fontSize = 200;
+                $textBoundingBox = imagettfbbox($fontSize, 0, __DIR__ . '/font/Roboto-Regular.ttf', $text);
+                // decrease the default font size until it fits nicely within the image - Code adapted from https://github.com/img-src/placeholder
+                while (((($width - ($textBoundingBox[2] - $textBoundingBox[0])) < 10) || (($height - ($textBoundingBox[1] - $textBoundingBox[7])) < 10)) && ($fontSize > 1)) {
+                    $fontSize --;
+                    $textBoundingBox = imagettfbbox($fontSize, 0, __DIR__ . '/font/Roboto-Regular.ttf', $text);
+                }
+                imagettftext($image, $fontSize, 0, ($width / 2) - (($textBoundingBox[2] - $textBoundingBox[0]) / 2), ($height / 2) + (($textBoundingBox[1] - $textBoundingBox[7]) / 2), $text_color, __DIR__ . '/font/Roboto-Regular.ttf', $text);
             }
-            imagettftext($image, $fontSize, 0, ($width / 2) - (($textBoundingBox[2] - $textBoundingBox[0]) / 2), ($height / 2) + (($textBoundingBox[1] - $textBoundingBox[7]) / 2), $text_color, __DIR__ . '/font/Roboto-Regular.ttf', $word);
 
             switch ($format) {
                 case 'jpg':
